ac7c8ebc90bcefd9e38637ef8cf3d50951072ff5
Matthew Anderson <mattlokk@gmail.com>
2018-12-13 10:39:54 -0800

    hooked up back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import { Container } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,12 +14,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-
-        <ShoppingList></ShoppingList>
-      </div>
-
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList></ShoppingList>
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
