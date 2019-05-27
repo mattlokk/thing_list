9fd46abb619366bab03cee68678aa31bfdf36275
matthew <mattlokk@gmail.com>
2019-05-27 16:16:55 -0700

    fixing

--- client/src/store.js
+++ client/src/store.js
@@ -10,8 +10,4 @@ const store = createStore(rootReducer, initialState, compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
-<<<<<<< HEAD
-export default store;
-=======
-export default store;
->>>>>>> 02ccf8a54a90ab3aea293792f3a41d909965e5c3
+
